d8ed94101e0a9ff8f29bf1787df72741fc4cdf5c
Breeanna Fout <breefout@gmail.com>
2020-06-21 21:10:18 -0500

    Phase Three

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,8 +10,11 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            films: [],  
-            loading: false
+            films: [],
+            loadingFilm: false,
+            people: [],
+            loadingPeople: false
+
         };
     }
 
@@ -19,29 +22,48 @@ export default class App extends React.Component {
         fetch("https://ghibliapi.herokuapp.com/films")
             .then(res => res.json())
             .then(arr => this.setState({ films: arr }))
+        fetch("https://ghibliapi.herokuapp.com/people")
+            .then(res => res.json())
+            .then(arr => this.setState({ people: arr }))
     }
 
 
 
     render() {
-        if (this.state.loading) {
-            return <div class="myContainer">
+        if (this.state.loadingFilm) {
+            return <div><img src={logo} /><div class="myContainer">
                 {this.state.films.map((film) => <div class="card" style={{ width: "18rem" }}>
                     <div class="card-body">
                         <h5 class="card-title">{film['title']}</h5>
                         <p class="card-text">{film['description']}</p>
                     </div>
                 </div>)}
+            </div>
             </div>;
 
 
+        } else if (this.state.loadingPeople) {
+            return <div><img src={logo} /> <div class="myContainer">
+                {this.state.people.map((person) => <div class="card" style={{ width: "18rem" }}>
+                    <div class="card-body">
+                        <h5 class="card-title">{person['name']}</h5>
+                        <p class="card-text">{person['age']}</p>
+                        <p class="card-text">{person['gender']}</p>
+                    </div>
+                </div>)}
+            </div>
+            </div>;
         } else {
             return <div>
-                <Button onClick={() => this.setState({loading: true})} style={{margin:'20px'}} variant="primary">Load</Button>
+                <img src={logo} />
+                <Button onClick={() => this.setState({ loadingFilm: true })} style={{ margin: '20px' }} variant="primary">Load Films</Button>
+                
+                <Button onClick={() => this.setState({ loadingPeople: true })} style={{ margin: '20px' }} variant="primary">Load People</Button>
             </div>
 
-        } 
-        
+
+        }
+
 
     }
 }
